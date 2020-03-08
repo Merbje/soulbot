4e3c7449d5a840bb06aba4c2575514a070e269b3
Merbje <50573135+Merbje@users.noreply.github.com>
2020-03-08 18:19:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,8 @@ let config = {
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    max_connections = 512
 };
 
 let con = mysql.createConnection(config);
