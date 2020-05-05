37e843c86dc20fada2927a6821f07513bf8d0adf
bram <bdjschoolmail@gmail.com>
2020-05-05 11:13:24 +0200

    Removed kili

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ discord.on('ready', () => {
 });
 
 discord.on('message', msg => {
-    console.log(msg.channel);
+    console.log(msg.channel["TextChannel"]['name']);
     if (msg.content.startsWith('!')) {
         if (oneconnect === false) {
             oneconnect = true;
