8f0b2a525d73658060f3cd76d57034062f74774f
bram <bdjschoolmail@gmail.com>
2020-05-26 23:22:47 +0200

    Working on storycommands

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ discord.on('ready', () => {
         setInterval(function(){
             queryRun('select * from events', (events) => {
                 let date = new Date();
-                console.log('date ' + date);
+                console.log('date ' + date.toISOString());
                 for (let i = 0; i < events.length; i++) {
                     console.log(events[i]);
                     if (date.toISOString() > events[i].time) {
