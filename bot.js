d8098a52d03180015cc575223af6c855d3808a4e
bram <bdjschoolmail@gmail.com>
2020-05-27 17:08:03 +0200

    Working on storycommands

--- bot.js
+++ bot.js
@@ -67,6 +67,8 @@ let client = new pg.Client(conString);
 
 discord.on('ready', () => {
     console.log('Logged in as soulBotForDofus!');
+    discord.channels.get(requirements).send(`__**Sheduled Event**__\n<@${ultra}> and <@${chev}> are organizing a **Tynril dungeon farming session** from **19:30** till approximately **22:00** (dofus time). Everyone is welcome to join. Be sure to always have keys ready.  Participants may be replaced halfway throughout the event if there is high demand. \nRespond with a <:plusone:674594462726357012> if you would like to join.\n<@member><@friend>`).then(reactions => { reactions.react(plusone).catch();});
+
         var dayMillseconds = 1000 * 60 * 5;
         setInterval(function(){
             if(moment.tz('UTC').day() === 1 && moment.tz('Europe/Paris').hour() === 10 && moment.tz('Europe/Paris').minute() < 5) {
