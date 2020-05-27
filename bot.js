b8c3bf5c45dd31c94f9b754bece0784c14b79e37
bram <bdjschoolmail@gmail.com>
2020-05-27 13:27:52 +0200

    Working on storycommands

--- bot.js
+++ bot.js
@@ -65,8 +65,7 @@ discord.on('ready', () => {
         var dayMillseconds = 1000 * 5;
         setInterval(function(){
             queryRun('select * from events', (events) => {
-                let currentTime = moment.tz('UTC');
-                moment.add(-2, 'hours');
+                let currentTime = moment().add(-2, 'hours');
                 for (let i = 0; i < events.length; i++) {
                     const eventTime = moment.tz(events[i].time, 'UTC');
                     console.log(`Eventtime ${eventTime.format()} and Currenttime ${currentTime.format()}`)
