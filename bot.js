3dcd6e45688d6006cca3537ce857ce81e965552b
bram <bdjschoolmail@gmail.com>
2020-05-27 15:47:45 +0200

    Working on storycommands

--- bot.js
+++ bot.js
@@ -23,6 +23,11 @@ const vinkje = process.env.VINKJE;
 const cross = process.env.CROSS;
 const plusone = process.env.PLUSONE;
 
+//People
+const ultra = process.env.ULTRA;
+const chev = process.env.CHEV;
+const panda = process.env.PANDA;
+
 //Ranks
 const farmrole = process.env.FARMROLE;
 
@@ -68,12 +73,15 @@ discord.on('ready', () => {
             console.log(moment.tz('Europe/Paris').minute() < 10);
             //test
             if(moment.tz('UTC').day() === 3 && moment.tz('Europe/Paris').hour() === 15 && moment.tz('Europe/Paris').minute() < 15) {
-                msg.client.channels.get(requirements).send(`**Sheduled Event!**\n\n Instinct-Ultra and Chevulmi are organizing a Tynril farm session at  19:30 .\nRespond with a +1 if you would like to join.`).then(reactions => { reactions.react(plusone).catch();
+                const now = moment.tz('Europe/Paris');
+                let xxx = now.format('YYYY-MM-DD') + 'T19:30:00';
+                const eventTime = moment.tz(xxx, 'Europe/Paris').tz('UTC').format('YYYY-MM-DD hh:mm:ss');
+                msg.client.channels.get(requirements).send(`**Sheduled Event!**\n<@${ultra}>, <@${chev}> and a setup cra are organizing a Tynril dungeon farm from 19:30 to approximately 22:00 (dofus time). Everyone is welcome to join. Make sure to always have some keys ready. In case we get full, we will swap some people after 4-5 runs for others so more people can participate.\nRespond with a +1 if you would like to join.`).then(reactions => { reactions.react(plusone).catch();
                     insertNewEvent(`INSERT INTO events(messageID, time) VALUES ('${reactions.id}', '${eventTime}')`, () => {});
                 });
             }
             queryRun('select * from events', (events) => {
-                let currentTime = moment.tz('UTC').add(-4, 'hours');
+                let currentTime = moment.tz('Europe/Paris').add(-4, 'hours');
                 for (let i = 0; i < events.length; i++) {
                     const eventTime = moment.tz(events[i].time, 'UTC');
                     console.log(`Eventtime ${eventTime.format()} and Currenttime ${currentTime.format()}`)
