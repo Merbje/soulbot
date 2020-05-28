74759a224bd72d5bf35ebef967e539ba1220c3d7
bram <bdjschoolmail@gmail.com>
2020-05-28 12:48:35 +0200

    Adding status

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ let client = new pg.Client(conString);
 
 discord.on('ready', () => {
     console.log('Logged in as soulBotForDofus!');
-    discord.user.setStatus('Test', 'Bonasouls');
+    discord.user.setActivity('!help', {type: "LISTENING"});
     // discord.channels.get(requirements).send(`__**Sheduled Event**__\n<@${ultra}> and <@${chev}> are organizing a **Tynril dungeon farming session** from **19:30** till approximately **22:00** (dofus time). Everyone is welcome to join. Be sure to always have keys ready.  Participants may be replaced halfway throughout the event if there is high demand. \nRespond with a <:plusone:674594462726357012> if you would like to join.\n${memberrole}${friendrole}`).then(reactions => { reactions.react(plusone).catch();});
 
         var dayMillseconds = 1000 * 60;
