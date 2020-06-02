e9aba017cfc324040fef7e4c8828359010ca64d2
bram <bdjschoolmail@gmail.com>
2020-06-02 09:49:59 +0200

    2

--- bot.js
+++ bot.js
@@ -106,13 +106,6 @@ discord.on('ready', () => {
 
                 let currentTime = moment.tz('Europe/Paris').add(-4, 'hours');
                 for (let i = 0; i < events.length; i++) {
-                    message.channel.fetchMessage(events[i].messageid).then(themessage => {
-
-                        let MessageOBJ = themessage.reactions.find(r => r.name === plusone);
-
-                        console.log(MessageOBJ.users);
-
-                    });
                     const eventTime = moment.tz(events[i].time, 'UTC');
                     if (eventTime < currentTime) {
                         discord.channels.get(eventchannel).fetchMessage(events[i].messageid).then(msg => msg.delete());
@@ -124,6 +117,13 @@ discord.on('ready', () => {
 });
 
 discord.on('message', msg => {
+    msg.channel.fetchMessage('717267203631742977').then(themessage  => {
+
+        let MessageOBJ = themessage.reactions.find(r => r.name === plusone);
+
+        console.log(MessageOBJ.users);
+
+    });
     if (msg.channel.type !== "dm") {
         if (msg.content.startsWith('!')) {
             if (oneconnect === false) {
